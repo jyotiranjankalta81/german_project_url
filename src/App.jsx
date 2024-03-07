ed59155bccbed03faa73d97dd3ffd984180d3970
Jyoti Ranjan <18768380-jyotiranjankalt@users.noreply.replit.com>
2024-03-07 09:04:19 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import DraggableShape from './Dragger'
+
+export default function App() {
+  return (
+    <main>
+      {/* React ⚛️ + Vite ⚡ + Replit */}
+      <DraggableShape />
+    </main>
+  )
+}
